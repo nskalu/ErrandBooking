95e9f3754682151934049818be7c9b079c259a6c
Ngozi Kalu <kalungozi2@gmail.com>
2020-11-02 22:50:31 +0100

    addition of 2 more properties

--- src/Delivery.js
+++ src/Delivery.js
@@ -17,7 +17,9 @@ app.post('/api/make-booking', (req, res)=>{
         PickupAddress:req.body.PickupAddress,
         DeliveryAddress:req.body.DeliveryAddress,
         PickupPhone:req.body.PickupPhone,
-        DeliveryPhone:req.body.DeliveryPhone
+        DeliveryPhone:req.body.DeliveryPhone,
+        PickupDate:req.body.PickupDate,
+        DeliveryDate:req.body.DeliveryDate
     };
     errands.push(delivery);
     res.send(delivery);
@@ -45,13 +47,15 @@ app.put('/api/booking/:Id', (req, res)=>{
    const {error} = validateDelivery(req.body); //this line is equivalent to returning result.error, it's called object destructuring
     if (error) return res.status(400).send(error.details[0].message);
     
-    booking.Name=req.body.Name
-    booking.Phone=req.body.Phone
-    booking.PickupAddress=req.body.PickupAddress
-    booking.DeliveryAddress=req.body.DeliveryAddress
-    booking.Email=req.body.Email,
-    booking.PickupPhone=req.body.PickupPhone
-    booking.DeliveryPhone=req.body.DeliveryPhone
+    booking.Name=req.body.Name;
+    booking.Phone=req.body.Phone;
+    booking.PickupAddress=req.body.PickupAddress;
+    booking.DeliveryAddress=req.body.DeliveryAddress;
+    booking.Email=req.body.Email;
+    booking.PickupPhone=req.body.PickupPhone;
+    booking.DeliveryPhone=req.body.DeliveryPhone;
+    booking.DeliveryDate = req.body.DeliveryDate;
+    booking.PickupDate = req.body.PickupDate;
    
     res.send(errands);
 });
@@ -75,7 +79,9 @@ function validateDelivery(bookingModel){
         DeliveryAddress : Joi.string().required(),
         Email: Joi.string().required(),
         DeliveryPhone:Joi.string().required(),
-        PickupPhone:Joi.string().required()
+        PickupPhone:Joi.string().required(),
+        DeliveryDate:Joi.datetime().iso().required,
+        PickupDate:Joi.datetime().iso().required
     };
 
     return Joi.validate(bookingModel, schema);
@@ -85,6 +91,6 @@ function validateDelivery(bookingModel){
 //Use array to store data for now
 const errands = 
     [
-        {Id : 1, Name:'Chi Esther', Phone:"08099977876", PickupAddress:"Ikeja", DeliveryAddress:"Lagos Island", Email:"ng@yahoo.com",PickupPhone:"08090977876",DeliveryPhone:"08099923876"}, 
-        {Id : 2, Name:'John Omega', Phone:"08098977876", PickupAddress:"Gbagada", DeliveryAddress:"Lagos Island", Email:"babyng@yahoo.com",PickupPhone:"09090977126",DeliveryPhone:"08092312876"}
+        {Id : 1, Name:'Chi Esther', Phone:"08099977876", PickupAddress:"Ikeja", DeliveryAddress:"Lagos Island", Email:"ng@yahoo.com",PickupPhone:"08090977876",DeliveryPhone:"08099923876",DeliveryDate:"11-02-2020",PickupDate:"11-02-2020"}, 
+        {Id : 2, Name:'John Omega', Phone:"08098977876", PickupAddress:"Gbagada", DeliveryAddress:"Lagos Island", Email:"babyng@yahoo.com",PickupPhone:"09090977126",DeliveryPhone:"08092312876",DeliveryDate:"11-02-2020",PickupDate:"11-02-2020"}
     ]
