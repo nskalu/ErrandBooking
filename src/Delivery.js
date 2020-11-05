faf7a4d53d7bad577786ed26f84cbd400e4bd28d
Ngozi Kalu <kalungozi2@gmail.com>
2020-11-05 23:48:04 +0100

    adds an environment variable for

--- src/Delivery.js
+++ src/Delivery.js
@@ -8,7 +8,8 @@ app.post('/api/make-booking', (req, res)=>{
     const {error} = validateDelivery(req.body); //this line is equivalent to returning result.error, it's called object destructuring
     if (error) return res.status(400).send(error.details[0].message);
  
-
+    console.log(req.body.Name);
+//set the delivery object from the request
     const delivery= {
         Id:errands.length + 1, 
         Name:req.body.Name,
@@ -57,7 +58,7 @@ app.put('/api/booking/:Id', (req, res)=>{
     booking.DeliveryDate = req.body.DeliveryDate;
     booking.PickupDate = req.body.PickupDate;
    
-    res.send(errands);
+    res.send(booking);
 });
 //delete a course
 app.delete('/api/booking/:Id', (req, res)=>{
@@ -94,3 +95,7 @@ const errands =
         {Id : 1, Name:'Chi Esther', Phone:"08099977876", PickupAddress:"Ikeja", DeliveryAddress:"Lagos Island", Email:"ng@yahoo.com",PickupPhone:"08090977876",DeliveryPhone:"08099923876",DeliveryDate:"11-02-2020",PickupDate:"11-02-2020"}, 
         {Id : 2, Name:'John Omega', Phone:"08098977876", PickupAddress:"Gbagada", DeliveryAddress:"Lagos Island", Email:"babyng@yahoo.com",PickupPhone:"09090977126",DeliveryPhone:"08092312876",DeliveryDate:"11-02-2020",PickupDate:"11-02-2020"}
     ]
+
+    //setting an environment variable
+    const port = process.env.PORT || 3000;
+    app.listen(port, ()=>{ console.log(`listening on port ${port}`)});
