e1fb0d1f147228b0b45f5b9e1c775e6ed3866360
Ngozi Kalu <kalungozi2@gmail.com>
2020-11-21 11:50:25 +0100

    small corrections

--- src/controller.js
+++ src/controller.js
@@ -1,16 +1,18 @@
 
 const express = require('express') //returns a function
 const app = express();
-const db = require('../shared/db');
-const validate = require ('../shared/validate');
+const db = require('db.js');
+const validate = require ('./src/shared/validate');
 app.use(express.json());
 
 //POST: make a delivery booking
-app.post('/api/make-booking', (req, res)=>{ 
+app.post('/api/make-booking', async function(req, res){ 
+    console.log("just hitting");
     const {error} = validate.validateDelivery(req.body); //this line is equivalent to returning result.error, it's called object destructuring
     if (error) return res.status(400).send(error.details[0].message);
+    console.log("validated successfully");
     const result = await db.createErrand(req.body);
-    
+    console.log(result);
     res.send(result);
 });
 
